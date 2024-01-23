df69c9a31ba5edc15cddf1d561bb5ab60081c1bb
Jack Bowlin <jrbowlin7@gmail.com>
2024-01-23 09:17:59 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for
+
+import sqlite3
+
+
+
+
+
+app = Flask(__name__, static_url_path='/static')
+
+
+def get_db_connection():
+    conn = sqlite3.connect('jack.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route("/")
+def runSite():
+    conn = get_db_connection()
+    projects = conn.execute("SELECT name, description FROM projects").fetchall()
+    conn.close()
+    print(projects)
+    return render_template('resume.html', projects=projects)
+
+
